4a8c1a3f96a72cb2aa01382034e6787bd5177467
dzoric1 <Gerasimov2099@mail.ru>
2024-05-05 17:26:43 +0800

    initial and base style structure

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,32 @@
+<script setup></script>
+
+<template>
+  <header class="header">
+    <RouterLink to="./" class="header__logo">
+      <img src="@/assets/logo.svg" alt="логотип" />
+    </RouterLink>
+    <button class="header__catalog-button">
+      <img src="@/assets/catalog.svg" alt="иконка кнопки" />
+      Каталог
+    </button>
+    <p>Поиск</p>
+    <a href="#">link</a>
+  </header>
+</template>
+
+<style lang="scss">
+.header {
+  padding: 26px 0;
+  display: flex;
+  gap: 10px;
+}
+
+.header__catalog-button {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  font-size: 15px;
+  padding: 16px;
+  background-color: var(--red);
+}
+</style>
