af83f6531cfad942017880a1294a4e5bd041f391
dzoric1 <Gerasimov2099@mail.ru>
2024-05-05 21:46:53 +0800

    realized header layout

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,24 +1,119 @@
-<script setup></script>
+<script setup>
+import SearchForm from './SearchForm.vue'
+import IconUser from './icons/IconUser.vue'
+import IconOrders from './icons/IconOrders.vue'
+import IconFavorites from './icons/IconFavorites.vue'
+import IconCart from './icons/IconCart.vue'
+</script>
 
 <template>
   <header class="header">
-    <RouterLink to="./" class="header__logo">
-      <img src="@/assets/logo.svg" alt="логотип" />
-    </RouterLink>
-    <button class="header__catalog-button">
-      <img src="@/assets/catalog.svg" alt="иконка кнопки" />
-      Каталог
-    </button>
-    <p>Поиск</p>
-    <a href="#">link</a>
+    <div class="header__container container">
+      <RouterLink to="./" class="header__logo">
+        <img src="@/assets/logo.svg" alt="логотип" />
+      </RouterLink>
+      <div class="header__row">
+        <button class="header__catalog-button" type="button">
+          <img src="@/assets/catalog.svg" alt="иконка кнопки" />
+          Каталог
+        </button>
+        <SearchForm class="header__search" placeholder="Поиск по товарам" />
+      </div>
+      <nav class="header__nav">
+        <ul class="header__nav-list">
+          <li class="header__nav-item">
+            <RouterLink to="./sign-in" class="header__nav-link">
+              <span class="header__nav-count"></span>
+              <IconUser />
+              Войти
+            </RouterLink>
+          </li>
+          <li class="header__nav-item">
+            <RouterLink to="./orders" class="header__nav-link">
+              <span class="header__nav-count"></span>
+              <IconOrders />
+              Заказы
+            </RouterLink>
+          </li>
+          <li class="header__nav-item">
+            <RouterLink to="./favorites" class="header__nav-link">
+              <span class="header__nav-count">2</span>
+              <IconFavorites />
+              Избранное
+            </RouterLink>
+          </li>
+          <li class="header__nav-item">
+            <RouterLink to="./cart" class="header__nav-link">
+              <span class="header__nav-count">22</span>
+              <IconCart />
+              Корзина
+            </RouterLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </header>
 </template>
 
 <style lang="scss">
 .header {
   padding: 26px 0;
+  background-color: var(--white);
+  box-shadow: 0 2px 18px 0 rgba(12, 12, 13, 0.1);
+
+  @include tablet {
+    position: fixed;
+    top: 0;
+    width: 100%;
+    padding: 10px 0;
+    z-index: 100;
+    border-bottom: 2px solid var(--text-gray);
+  }
+}
+
+.header__search {
+  max-width: 566px;
+  margin-right: 15px;
+
+  @include tablet {
+    margin-right: 0;
+  }
+}
+
+.header__container {
   display: flex;
-  gap: 10px;
+  align-items: center;
+
+  @include tablet {
+    flex-direction: column;
+    gap: 12px;
+  }
+}
+
+.header__row {
+  display: flex;
+
+  width: 100%;
+
+  @include tablet {
+    justify-content: center;
+  }
+}
+
+.header__logo {
+  display: block;
+  width: 100%;
+  max-width: 202px;
+  margin-right: 58px;
+
+  img {
+    width: 100%;
+    object-fit: cover;
+  }
+
+  @include tablet {
+    margin-right: 0;
+  }
 }
 
 .header__catalog-button {
@@ -26,7 +121,70 @@
   align-items: center;
   gap: 12px;
   font-size: 15px;
-  padding: 16px;
   background-color: var(--red);
+  color: var(--white);
+  border-radius: 9px;
+  padding: 12px 16px;
+  width: 100%;
+  max-width: 120px;
+  margin-right: -15px;
+  z-index: 1;
+  transition: opacity 0.2s ease;
+
+  &:hover {
+    opacity: 0.6;
+  }
+
+  @include tablet {
+    gap: 5px;
+    padding: 8px;
+    max-width: max-content;
+    font-size: 14px;
+  }
+}
+
+.header__nav {
+  margin-left: auto;
+
+  @include tablet {
+    margin-left: 0;
+  }
+}
+
+.header__nav-list {
+  display: flex;
+  gap: 30px;
+}
+
+.header__nav-item {
+  font-size: 12px;
+}
+
+.header__nav-link {
+  padding-top: 5px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  svg {
+    width: 25px;
+    height: 25px;
+    object-fit: contain;
+  }
+}
+
+.header__nav-count {
+  position: absolute;
+  top: 0;
+  right: 50%;
+  transform: translateX(110%);
+  background-color: var(--red);
+  font-size: 11px;
+  padding: 4px;
+  border-radius: 44px;
+  color: var(--white);
+  line-height: 75%;
 }
 </style>
---
+++ src/components/SearchForm.vue
@@ -0,0 +1,95 @@
+<script setup>
+import IconSearch from './icons/IconSearch.vue'
+
+const props = defineProps({
+  class: String,
+  placeholder: String
+})
+</script>
+
+<template>
+  <form class="search" :class="props.class">
+    <div class="search__input-wrapper">
+      <input
+        class="search__input"
+        type="text"
+        :placeholder="props.placeholder"
+      />
+      <button class="search__submit" type="submit" aria-label="Поиск">
+        <IconSearch />
+      </button>
+    </div>
+  </form>
+</template>
+
+<style lang="scss">
+.search {
+  width: 100%;
+
+  &:focus-within {
+    .search__input-wrapper {
+      background: var(--red);
+    }
+    .search__input {
+      color: var(--white);
+      &::placeholder {
+        color: var(--white);
+        opacity: 0.5;
+      }
+    }
+    .search__submit {
+      svg {
+        path {
+          stroke: var(--white);
+        }
+      }
+    }
+  }
+}
+
+.search__input-wrapper {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  transition: background 0.2s ease;
+  background: rgba(93, 136, 150, 0.08);
+  border-radius: 9px;
+}
+
+.search__input {
+  background-color: transparent;
+  border-radius: 9px;
+  border: none;
+  width: 100%;
+  padding: 12px 12px 12px 27px;
+  font-size: 15px;
+  color: var(--text-black);
+
+  &::placeholder {
+    color: var(--text-gray);
+  }
+
+  @include tablet {
+    padding: 8px 8px 8px 27px;
+    font-size: 14px;
+  }
+}
+
+.search__submit {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 50px;
+  transition: opacity 0.2s ease;
+
+  svg {
+    width: 18px;
+    height: 18px;
+    object-fit: contain;
+  }
+
+  &:hover {
+    opacity: 0.6;
+  }
+}
+</style>
