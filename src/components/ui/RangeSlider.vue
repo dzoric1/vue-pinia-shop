b507538c775124fb81f1ca29af5a7679b9f15c28
dzoric1 <Gerasimov2099@mail.ru>
2024-05-06 23:40:28 +0800

    refactor styles range-slider

--- src/components/ui/RangeSlider.vue
+++ src/components/ui/RangeSlider.vue
@@ -1,86 +1,90 @@
-<script>
+<script setup>
 import { ref, computed } from 'vue'
 
-export default {
-  setup() {
-    const minAngle = ref(0)
-    const maxAngle = ref(480)
-    const startBackground = ref('0')
-    const widthBackground = ref('100%')
+const minAngle = ref(0)
+const maxAngle = ref(480)
+const startBackground = ref('0')
+const widthBackground = ref('100%')
 
-    const sliderMin = computed({
-      get: () => minAngle.value,
-      set: (val) => {
-        val = parseInt(val)
-        if (val > maxAngle.value) {
-          maxAngle.value = val
-        }
-        minAngle.value = val
-        console.log(val / 480)
-        startBackground.value = `${(val / 480) * 100}%`
-        widthBackground.value = `${(maxAngle.value / 480) * 100 - (minAngle.value / 480) * 100}%`
-      }
-    })
-
-    const sliderMax = computed({
-      get: () => maxAngle.value,
-      set: (val) => {
-        val = parseInt(val)
-        if (val < minAngle.value) {
-          minAngle.value = val
-        }
-        maxAngle.value = val
-        console.log(val / 480)
-        startBackground.value = `${(minAngle.value / 480) * 100}%`
-        widthBackground.value = `${(val / 480) * 100 - (minAngle.value / 480) * 100}%`
-      }
-    })
-
-    return {
-      minAngle,
-      maxAngle,
-      sliderMin,
-      sliderMax,
-      // backgroundStart,
-      // backgroundEnd,
-      startBackground,
-      widthBackground
+const sliderMin = computed({
+  get: () => minAngle.value,
+  set: (val) => {
+    val = parseInt(val)
+    if (val > maxAngle.value) {
+      maxAngle.value = val
     }
+    if (val < 0) {
+      minAngle.value = 0
+    } else {
+      minAngle.value = val
+    }
+    startBackground.value = `${(val / 480) * 100}%`
+    widthBackground.value = `${(maxAngle.value / 480) * 100 - (minAngle.value / 480) * 100}%`
   }
-}
+})
+
+const sliderMax = computed({
+  get: () => maxAngle.value,
+  set: (val) => {
+    val = parseInt(val)
+    if (val < minAngle.value) {
+      minAngle.value = val
+    }
+    if (val > 480) {
+      maxAngle.value = 480
+    } else {
+      maxAngle.value = val
+    }
+    startBackground.value = `${(minAngle.value / 480) * 100}%`
+    widthBackground.value = `${(val / 480) * 100 - (minAngle.value / 480) * 100}%`
+  }
+})
 </script>
 
 <template>
   <div class="range-slider">
-    <input
-      type="range"
-      min="0"
-      max="480"
-      step="10"
-      v-model.number="sliderMin"
-    />
-    <input
-      type="number"
-      min="0"
-      max="480"
-      step="10"
-      v-model.number="sliderMin"
-    />
-    <input
-      type="range"
-      min="0"
-      max="480"
-      step="10"
-      v-model.number="sliderMax"
-    />
-    <input
-      type="number"
-      min="0"
-      max="480"
-      step="10"
-      v-model.number="sliderMax"
-    />
-    <span class="range-slider__active"></span>
+    <h3 class="range-slider__title">Вес</h3>
+    <div class="range-slider__numbers">
+      <div class="range-slider__input-number">
+        <span>от</span>
+        <input
+          type="number"
+          min="0"
+          max="480"
+          step="1"
+          v-model.number="sliderMin"
+        />
+        <span>г</span>
+      </div>
+      <div class="range-slider__input-number">
+        <span>до</span>
+        <input
+          type="number"
+          min="0"
+          max="480"
+          step="1"
+          v-model.number="sliderMax"
+        />
+        <span>г</span>
+      </div>
+    </div>
+    <div class="range-slider__range">
+      <input
+        type="range"
+        min="0"
+        max="480"
+        step="10"
+        v-model.number="sliderMin"
+      />
+      <input
+        type="range"
+        min="0"
+        max="480"
+        step="10"
+        v-model.number="sliderMax"
+      />
+      <span class="range-slider__active-bar"></span>
+    </div>
   </div>
 </template>
 
@@ -91,7 +95,37 @@ export default {
   margin: auto;
   text-align: center;
   position: relative;
-  height: 6em;
+}
+
+.range-slider__title {
+  text-align: left;
+  font-weight: 600;
+  font-size: 18px;
+  margin-bottom: 20px;
+}
+
+.range-slider__range {
+  position: relative;
+}
+
+.range-slider__numbers {
+  display: flex;
+  width: 100%;
+  gap: 7px;
+  color: var(--text-gray);
+  margin-bottom: 23px;
+}
+
+.range-slider__input-number {
+  width: 100%;
+  display: grid;
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 5px;
+  border-radius: 6px;
+  background: rgba(93, 136, 150, 0.08);
 }
 
 .range-slider input[type='range'] {
@@ -100,36 +134,32 @@ export default {
   bottom: 0;
 }
 
-.range-slider__active {
+.range-slider__active-bar {
   position: absolute;
   left: 0;
   bottom: 0;
   height: 3px;
   width: 100%;
   background: var(--light-green);
-  /* background: linear-gradient(to right, #ff0000 50%, #0000ff 50%); */
-  /* left: 41.2%; */
   left: v-bind(startBackground);
   width: v-bind(widthBackground);
+  max-width: 100%;
 }
 
 input[type='number'] {
-  padding: 10px;
-  border: none;
-  border-radius: 6px;
   text-align: center;
+  border: none;
+  background: transparent;
+  width: 100%;
+  font-size: 14px;
+  line-height: 120%;
   color: var(--text-gray);
-  background: rgba(93, 136, 150, 0.08);
-}
-
-input[type='number']::-webkit-outer-spin-button,
-input[type='number']::-webkit-inner-spin-button {
-  -webkit-appearance: none;
 }
 
 input[type='number']:invalid,
 input[type='number']:out-of-range {
-  border: 2px solid var(--red);
+  font-weight: 600;
+  color: var(--red);
 }
 
 input[type='range'] {
@@ -150,8 +180,8 @@ input[type='range']::-webkit-slider-runnable-track {
 input[type='range']::-webkit-slider-thumb {
   z-index: 2;
   position: relative;
-  height: 19px;
-  width: 19px;
+  height: 18px;
+  width: 18px;
   border-radius: 100%;
   background: #fff;
   box-shadow: 0 2px 11px 0 rgba(12, 12, 13, 0.2);
